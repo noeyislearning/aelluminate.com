2ebfded15d486771bc4c39b5066f79cd56ba4ec4
Noey Ignacio <ignacio.francis@aelluminate.com>
2024-11-11 19:04:57 +0800

    feat: add utility files for Tailwind CSS merging, Google Fonts, and internationalization

---
+++ utils/fonts.ts
@@ -0,0 +1,16 @@
+import { Lora, Lexend_Deca, Inter } from "next/font/google"
+
+export const lora = Lora({
+  subsets: ["latin"],
+  variable: "--font-lora",
+})
+
+export const lexend = Lexend_Deca({
+  subsets: ["latin"],
+  variable: "--font-lexend",
+})
+
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+})
