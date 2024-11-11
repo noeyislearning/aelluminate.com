d09451fcd933d19fa4a3d2953b881f27f479a8e4
Noey Ignacio <ignacio.francis@aelluminate.com>
2024-11-12 01:43:28 +0800

    feat: add button variants component and update footer layout with improved styling

--- components/index.tsx
+++ components/index.tsx
@@ -2,6 +2,7 @@
 export { default as GridPattern } from "./shared/patterns/grid"
 export { default as IconContainer } from "./shared/utils/icon-container"
 export { default as NavLogo } from "./shared/nav/logo"
+export { default as ButtonVariants } from "./shared/button/variants"
 
 /* UI Components */
 export { default as HomeHero } from "./ui/home-hero"
