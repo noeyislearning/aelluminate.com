33f330fb243e98d55872139e84c00ee200b1af8c
Noey Ignacio <ignacio.francis@aelluminate.com>
2024-11-11 21:36:48 +0800

    feat: create navLinks and socialLinks in links.ts; remove deprecated nav-links.ts

---
+++ lib/links.ts
@@ -0,0 +1,47 @@
+import { LinkItemType } from "fumadocs-ui/layouts/links"
+
+export const navLinks: LinkItemType[] = [
+  {
+    text: "/hive",
+    url: "/hive",
+    active: "nested-url",
+  },
+  {
+    text: "/labs",
+    url: "/labs",
+    active: "nested-url",
+  },
+]
+
+export const socialLinks = [
+  {
+    text: "/hive",
+    url: "https://github.com/aelluminate-hive",
+    icon: "RiGithubLine",
+  },
+  {
+    text: "/labs",
+    url: "https://github.com/aelluminate-labs",
+    icon: "RiGithubLine",
+  },
+  {
+    text: "",
+    url: "https://x.com/aelluminate",
+    icon: "RiTwitterXLine",
+  },
+  {
+    text: "",
+    url: "https://linkedin.com/company/aelluminate",
+    icon: "RiLinkedinBoxLine",
+  },
+  {
+    text: "",
+    url: "https://instagram.com/aelluminate",
+    icon: "RiInstagramLine",
+  },
+  {
+    text: "",
+    url: "https://threads.net/aelluminate",
+    icon: "RiThreadsLine",
+  },
+]
