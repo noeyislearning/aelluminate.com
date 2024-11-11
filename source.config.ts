752b418a8c701c1835fd4b0f7d915a3e1ea3d0f8
Noey Ignacio <ignacio.francis@aelluminate.com>
2024-11-11 21:35:59 +0800

    feat: enhance source.config.ts with MDX options and remark plugins for improved documentation generation

--- source.config.ts
+++ source.config.ts
@@ -1,5 +1,19 @@
 import { defineDocs, defineConfig } from "fumadocs-mdx/config"
+import { fileGenerator, remarkDocGen, remarkInstall, typescriptGenerator } from "fumadocs-docgen"
 
 export const { docs, meta } = defineDocs()
 
-export default defineConfig()
+export default defineConfig({
+  mdxOptions: {
+    rehypeCodeOptions: {
+      themes: {
+        light: "min-light",
+        dark: "github-dark-dimmed",
+      },
+    },
+    remarkPlugins: [
+      [remarkInstall, { Tabs: "InstallTabs" }],
+      [remarkDocGen, { generators: [typescriptGenerator(), fileGenerator()] }],
+    ],
+  },
+})
