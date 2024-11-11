85acfad12f9e61f90b94126dfa495e83985cf9d7
Noey Ignacio <ignacio.francis@aelluminate.com>
2024-11-11 21:36:17 +0800

    feat: refactor HomePage layout with new components and improved structure

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,23 +1,19 @@
 import * as React from "react"
 
 import { HomeLayout } from "fumadocs-ui/layouts/home"
-import { navLinks } from "@/lib/nav-links"
-import { HomeHero } from "@/components"
+import { navLinks } from "@/lib/links"
+import { NavLogo, HomeHero, Footer } from "@/components"
 
 export default function HomePage() {
   return (
-    <HomeLayout
-      nav={{ title: <p>Aelluminate</p> }}
-      links={navLinks}
-      githubUrl="https://github.com/LiterateInk"
-    >
-      <div className="grid min-h-screen" style={{ gridTemplateRows: "1fr auto" }}>
-        <main className="relative w-full space-y-16 overflow-hidden px-4 pb-48 pt-8 lg:px-0">
-          <section className="mx-auto w-full max-w-6xl overflow-x-hidden">
+    <HomeLayout nav={{ title: <NavLogo /> }} links={navLinks} className="container h-full">
+      <div className="grid h-full" style={{ gridTemplateRows: "1fr auto" }}>
+        <main className="relative w-full overflow-hidden px-4 pt-8 lg:px-0">
+          <section className="mx-auto w-full overflow-x-hidden">
             <HomeHero />
           </section>
-          <div className="absolute -bottom-12 left-0 right-0 h-[150px] w-full rounded-full bg-[hsl(var(--hue),60,40)]/30 blur-[100px] transition-colors dark:bg-[hsl(var(--hue),50,60)]/60" />
         </main>
+        <Footer />
       </div>
     </HomeLayout>
   )
